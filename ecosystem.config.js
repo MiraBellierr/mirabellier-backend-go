139803fe9b6c5742266a2ba5fb83da08ef2c6d13
mira <mira-chan1@outlook.com>
2026-06-14 21:40:09 +0800

    deploy settings ✨

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,26 +4,19 @@ module.exports = {
       name: "mirabellier-go",
       script: "./server",
       cwd: "/srv/mirabellier-backend-go",
-      env: {
-        PORT: "5000",
-        DB_FILE: "./database.sqlite3",
-        IMAGES_DIR: "./images",
-        GIN_MODE: "release",
-      },
-      // Restart if crash
+      env_file: ".env",
       autorestart: true,
       max_restarts: 10,
       restart_delay: 2000,
-      // Watch for file changes (disable in prod, enable for dev)
       watch: false,
-      // Logging
       out_file: "./logs/out.log",
       error_file: "./logs/err.log",
       log_date_format: "YYYY-MM-DD HH:mm:ss",
       merge_logs: true,
-      // Graceful shutdown
       kill_timeout: 5000,
       listen_timeout: 3000,
+      // Only use PM2's internal env vars, not the system's
+      // PM2_PUBLIC_KEY, etc.
     },
   ],
 };
